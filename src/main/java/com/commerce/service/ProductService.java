15d1c05401b8843df7c9fe7cfb3b90f54ee76d43
liljaewayne <792068007@qq.com>
2017-12-06 16:47:15 +0800

    商品管理模块, 以及与ftp服务器的交互

---
+++ src/main/java/com/commerce/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.commerce.service;
+
+import com.commerce.common.ServerResponse;
+import com.commerce.pojo.Product;
+import com.commerce.vo.ProductDetailVo;
+import com.github.pagehelper.PageInfo;
+
+public interface ProductService {
+
+    ServerResponse saveOrUpdateProduct(Product product);
+
+    ServerResponse<String> setSaleStatus(Integer productId, Integer status);
+
+    ServerResponse<ProductDetailVo> manageProductDetail(Integer productId);
+
+    ServerResponse<PageInfo> getProductList(int pageNum, int pageSize);
+
+    ServerResponse<PageInfo> searchProduct(String productName, Integer productId, int pageNum, int pageSize);
+
+    ServerResponse<ProductDetailVo> getProductDetail(Integer productId);
+
+    ServerResponse<PageInfo> getProductByKeywordCategory(String keyword, Integer categoryId, int pageNum, int pageSize, String orderBy);
+
+
+}
