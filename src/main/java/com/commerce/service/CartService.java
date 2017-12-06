13e0105e79c1fdd2196c988b66d2641ed59c8e04
liljaewayne <792068007@qq.com>
2017-12-06 16:21:47 +0800

    购物车模块

---
+++ src/main/java/com/commerce/service/CartService.java
@@ -0,0 +1,18 @@
+package com.commerce.service;
+
+import com.commerce.common.ServerResponse;
+import com.commerce.vo.CartVo;
+
+public interface CartService {
+    ServerResponse<CartVo> add(Integer userId, Integer productId, Integer count);
+
+    ServerResponse<CartVo> update(Integer userId, Integer productId, Integer count);
+
+    ServerResponse<CartVo> deleteProduct(Integer userId, String productIds);
+
+    ServerResponse<CartVo> list(Integer userId);
+
+    ServerResponse<CartVo> selectOrUnSelect(Integer userId, Integer productId, Integer checked);
+
+    ServerResponse<Integer> getCartProductCount(Integer userId);
+}
