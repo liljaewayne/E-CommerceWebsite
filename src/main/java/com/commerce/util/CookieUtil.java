d7dcd572b3a326e728d5bd0dd375450d6637db23
liljaewayne <792068007@qq.com>
2017-12-29 15:43:00 +0800

    add cookieUtil

---
+++ src/main/java/com/commerce/util/CookieUtil.java
@@ -0,0 +1,64 @@
+package com.commerce.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class CookieUtil {
+
+    private final static String COOKIE_DOMAIN = ".liujianwei.top";
+    private final static String COOKIE_NAME = "commerce_login_token";
+
+    public static String readLoginToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                log.info("read cookieName:{},cookieValue:{}", cookie.getName(), cookie.getValue());
+                if (StringUtils.equals(cookie.getName(), COOKIE_NAME)) {
+                    log.info("return cookieName:{},cookieValue:{}", cookie.getName(), cookie.getValue());
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void writeLoginToken(HttpServletResponse response, String token) {
+
+        Cookie cookie = new Cookie(COOKIE_NAME, token);
+        cookie.setDomain(COOKIE_DOMAIN);
+
+        // 设置在根目录
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        // 如果这个maxage不设置的话，cookie就不会写入硬盘，而是写在内存。只在当前页面有效。
+        cookie.setMaxAge(31_536_000/* 单位是秒, 一年 */);// 如果是-1, 代表永久
+
+        log.info("write cookieName:{},cookieValue:{}", cookie.getName(), cookie.getValue());
+
+        response.addCookie(cookie);
+    }
+
+
+    public static void delLoginToken(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (StringUtils.equals(cookie.getName(), COOKIE_NAME)) {
+                    cookie.setDomain(COOKIE_DOMAIN);
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);// 设置成0，代表删除此cookie。
+                    log.info("del cookieName:{},cookieValue:{}", cookie.getName(), cookie.getValue());
+                    response.addCookie(cookie);
+                    return;
+                }
+            }
+        }
+    }
+
+
+}
